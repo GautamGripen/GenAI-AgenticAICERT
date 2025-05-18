1b1d4e836dc7e2d567428cbbab6d00b1a9ba53be
gautam.kvafs@gmail.com <Nike@4231>
2025-05-18 15:18:24 +0530

    adding python app

--- App/api.py
+++ App/api.py
@@ -1,25 +1,29 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException   ##Importing FastAPI
 from starlette.responses import Response
 import uvicorn
-app = FastAPI()
-@app.get("/")
-def root():
+app = FastAPI()     ##to utilise FastAPI
+@app.get("/root")     ##@app.get is used to convert the function as an API. We can use Http methods like Get, POST, etc..
+def root():                                ## root is like a home page
     return {"message": "Fast API in python"}
 
-def add(a, b):
-    return a+b
+@app.post("/add") ## converting function add into API, we can give any name
+def add(a, b):           ## Defining the function
+    a = float(a)
+    b = float(b)
+    c = a + b
+    return c
 
+@app.post("/sub")
 def sub(a,b):
     c = a-b
-
     return c
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":           ##Everything should be included within "__main__" for the interpreter to understand
     # print(add(2,4.3))
     # print(sub(b=6, a=7))
     #
     # print("This is the initialization of my AI Journey")
 
-    uvicorn.run(app,port=5001)
+    uvicorn.run(app,port=5001)     ##Using the Fast API
 
